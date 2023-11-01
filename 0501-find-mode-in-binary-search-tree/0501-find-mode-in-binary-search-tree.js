497df31f14aebef6bb558b5a8b7c3dc4e7a6dcc6
Rashón (Teddy) Mitchell <rashonmitchell@users.noreply.github.com>
2023-11-01 16:11:01 -0500

    Time: 69 ms (68.91%), Space: 51.4 MB (22.27%) - LeetHub

---
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+const findMode = function(root) {
+  if(root == null) return []
+  const hash = {}
+  traverse(root, hash)
+  const res = Object.entries(hash).sort((a, b) => b[1] - a[1])
+  const result = [res[0][0]]
+  for(let i = 1; i < res.length; i++) {
+    if(res[i][1] === res[0][1]) result.push(res[i][0])
+    else break
+  }
+  return result
+};
+
+function traverse(node, hash) {
+  if(node === null) return
+  hash[node.val] = Object.prototype.hasOwnProperty.call(hash, node.val) ? hash[node.val] + 1 : 1
+  traverse(node.left, hash)
+  traverse(node.right, hash)
+}
